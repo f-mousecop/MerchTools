d299cb325f73cfe070222836805f83d395d5f84b
Charles Clark <charlesc489@gmail.com>
2026-03-01 15:37:12 -0500

    Change AlertDialog to custom Dialog to fix UI glitch on rotate in StoreCatalogScreen.kt

--- app/src/main/java/com/example/merchtools/ui/store/StoreCatalogScreen.kt
+++ app/src/main/java/com/example/merchtools/ui/store/StoreCatalogScreen.kt
@@ -1,23 +1,32 @@
 package com.example.merchtools.ui.store
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Store
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -30,11 +39,11 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -44,14 +53,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -75,17 +83,6 @@ fun StoreCatalogScreen(
     val uiEffect = viewModel.uiEffect
     val snackbarHostState = remember { SnackbarHostState() }
     val state by viewModel.state.collectAsStateWithLifecycle()
-
-    val keyboard = LocalSoftwareKeyboardController.current
-    val focusManger = LocalFocusManager.current
-    val configuration = LocalConfiguration.current
-
-    LaunchedEffect(configuration.orientation) {
-        if (state.isAddStoreDialogOpen) {
-            keyboard?.hide()
-            focusManger.clearFocus(force = true)
-        }
-    }
 
 
     /**
@@ -129,7 +126,6 @@ fun StoreCatalogScreen(
             onEvent = viewModel::onEvent,
             modifier = Modifier
                 .padding(innerPadding)
-                .imePadding()
         )
     }
 
@@ -279,50 +275,104 @@ fun StoreCard(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun AddStoreDialog(
     state: StoreState,
     onEvent: (StoreCatalogEvent) -> Unit
 ) {
-    AlertDialog(
+    Dialog(
         onDismissRequest = { onEvent(StoreCatalogEvent.HideAddStoreDialog) },
-        title = { Text("Add Store") },
-        text = {
-            OutlinedTextField(
-                value = state.newStoreName,
-                onValueChange = { onEvent(StoreCatalogEvent.OnStoreNameChanged(it)) },
-                label = { Text("Store name") },
-                supportingText = { Text("${state.newStoreName.length}/50") },
-                singleLine = true,
+        properties = DialogProperties(
+            dismissOnClickOutside = true,
+            usePlatformDefaultWidth = false,
+            decorFitsSystemWindows = false
+        ),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .imePadding()
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = { onEvent(StoreCatalogEvent.HideAddStoreDialog) }
+                )
+                .animateContentSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Surface(
+                shape = RoundedCornerShape(dimensionResource(R.dimen.padding_small)),
+                tonalElevation = dimensionResource(R.dimen.padding_small),
                 modifier = Modifier
-                    .fillMaxWidth()
-            )
-        },
-        confirmButton = {
-            TextButton(onClick = {
-                onEvent(StoreCatalogEvent.AddNewStore)
-            },
-                enabled = state.newStoreName.isNotBlank()
+                    .widthIn(max = 600.dp)
+                    .fillMaxWidth(0.8f)
+                    .padding(vertical = dimensionResource(R.dimen.padding_medium))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = { }
+                    ),
+                color = MaterialTheme.colorScheme.surface
             ) {
-                Text("Add")
+                Column(
+                    modifier = Modifier
+                        .padding(dimensionResource(R.dimen.padding_large))
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    Icon(
+                        Icons.Default.Store,
+                        contentDescription = "Add store",
+                        modifier = Modifier
+                            .size(48.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+
+                    Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+
+                    Text(
+                        text = "Add Store",
+                        style = MaterialTheme.typography.headlineSmall,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
+
+                    OutlinedTextField(
+                        value = state.newStoreName,
+                        onValueChange = { onEvent(StoreCatalogEvent.OnStoreNameChanged(it)) },
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        label = { Text("Store Name") },
+                        supportingText = { Text("${state.newStoreName.length}/50") },
+                        singleLine = true
+                    )
+
+                    Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_large)))
+
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.End)
+                    ) {
+                        TextButton(
+                            onClick = {
+                                onEvent(StoreCatalogEvent.HideAddStoreDialog)
+                            }
+                        ) { Text("Cancel") }
+
+                        TextButton(
+                            onClick = {
+                                onEvent(StoreCatalogEvent.AddNewStore)
+                            },
+                            enabled = state.newStoreName.isNotBlank()
+                        ) { Text("Add") }
+                    }
+
+                }
             }
-        },
-        dismissButton = {
-            TextButton(onClick = { onEvent(StoreCatalogEvent.HideAddStoreDialog) }
-            ) {
-                Text("Cancel")
-            }
-        },
-        icon = {
-            Icon(
-                Icons.Default.Store,
-                contentDescription = "Add store",
-                modifier = Modifier.size(48.dp)
-            )
-        },
-        shape = RoundedCornerShape(8.dp)
-    )
+        }
+    }
 }
 
 @Preview(
